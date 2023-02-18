e103958fe2b9463c3fee807afe1d046e3a428afb
LSX <96759883+LSX285@users.noreply.github.com>
2023-02-18 23:56:37 +0100

    fixed pathing

--- LTSC/Scripts/UpdateAPP.cmd
+++ LTSC/Scripts/UpdateAPP.cmd
@@ -1,7 +1,7 @@
 @echo off
 timeout 3
 ECHO [-] Updating APP ...
-attrib -r C:\Windows\Web\LTSC\App.bat
+attrib -r C:\Program Files\LTSC\App.bat
 powershell -command "wget https://raw.githubusercontent.com/LSX285/Windows11-LTSC/main/LTSC/App.bat -outfile 'C:\Program Files\LTSC\App.bat'"
 ECHO [+] Done. Open APP again.
 timeout 3
