bd8c7c9766b6b8ef5a5ef5343ca33f64557d4f39
LSX <96759883+LSX285@users.noreply.github.com>
2023-02-19 00:19:22 +0100

    Update UpdateAPP.cmd

--- LTSC/Scripts/UpdateAPP.cmd
+++ LTSC/Scripts/UpdateAPP.cmd
@@ -5,7 +5,7 @@ attrib -r C:\Program Files\LTSC\App.bat
 powershell -command "wget https://raw.githubusercontent.com/LSX285/Windows11-LTSC/main/LTSC/App.bat -outfile 'C:\Program Files\LTSC\App.bat'"
 ECHO [+] Done.
 timeout 3
-start cmd.exe @cmd "C:\Program Files\LTSC\App.bat"
+"C:\Program Files\LTSC\App.bat"
 exit
 
 
