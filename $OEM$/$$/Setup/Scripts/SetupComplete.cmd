d9a2e3980e49e8fc1ff4ab36a31ee1c6954e9833
LSX <96759883+LSX285@users.noreply.github.com>
2023-02-18 13:42:35 +0100

    Delete $OEM$/$$ directory

--- $OEM$/$$/Setup/Scripts/SetupComplete.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "RunOnce_RunMe" /t REG_SZ /d "\"C:\Windows\Web\LTSC\setup.cmd\"" /f >nul 2>&1
-				
-del /q /f "%0"
