d1a01279ff781c29277a5dc2b5eca02cbaebe295
LSX <LSX285p@gmail.com>
2023-02-28 04:42:01 +0100

    Updated APP updater

--- LTSC/Scripts/UpdateAPP.cmd
+++ LTSC/Scripts/UpdateAPP.cmd
@@ -1,6 +1,5 @@
 @echo off
 timeout 1 >nul 2>&1
-ECHO [[93m-[0m] [93mUpdating APP[0m ...
 attrib -r  C:\Program Files\LTSC\*.* /s >nul 2>&1
 powershell -command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/LSX285/Windows11-LTSC/main/LTSC/App.bat', 'C:\Program Files\LTSC\App.bat')" >nul 2>&1
 powershell -Command "[reflection.assembly]::loadwithpartialname('System.Windows.Forms'); [reflection.assembly]::loadwithpartialname('System.Drawing'); $notify = new-object system.windows.forms.notifyicon; $notify.icon = [System.Drawing.SystemIcons]::WinLogo; $notify.visible = $true; $notify.showballoontip(10,'APP Updater','APP has been updated to the latest version.',[system.windows.forms.tooltipicon]::None)" >nul 2>&1
