f55ab6a338c0d5ce5dae5cbffb45f4bd49707cac
LSX <96759883+LSX285@users.noreply.github.com>
2023-03-07 03:02:04 +0100

    Updated App

--- LTSC/App.bat
+++ LTSC/App.bat
@@ -40,7 +40,7 @@ REG Add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /V Prom
 :: Note - Welcome Page
 :Welcome
 mode con: cols=76 lines=20
-Title APP v1.0.2
+Title APP v1.0.3
 @ECHO OFF
 CLS
 @ECHO.   
