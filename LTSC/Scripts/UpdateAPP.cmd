466f912e69f0be3dd6e8fffc417f5dc806513f70
LSX <lsx285p@gmail.com>
2023-03-11 22:42:29 +0100

    Updated

--- LTSC/Scripts/UpdateAPP.cmd
+++ LTSC/Scripts/UpdateAPP.cmd
@@ -1,9 +1,13 @@
 @echo off
+
+set ltscdir=C:\Program Files\LTSC
+set repository=https://github.com/LSX285/Windows11-LTSC/raw/main
+
 timeout 1 >nul 2>&1
-attrib -r  C:\Program Files\LTSC\*.* /s >nul 2>&1
-powershell -command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/LSX285/Windows11-LTSC/main/LTSC/App.cmd', 'C:\Program Files\LTSC\App.cmd')" >nul 2>&1
+attrib -r  %ltscdir%\*.* /s >nul 2>&1
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/LSX285/Windows11-LTSC/raw/main/LTSC/App.cmd', '%ltscdir%\App.cmd')" >nul 2>&1
 powershell -Command "[reflection.assembly]::loadwithpartialname('System.Windows.Forms'); [reflection.assembly]::loadwithpartialname('System.Drawing'); $notify = new-object system.windows.forms.notifyicon; $notify.icon = [System.Drawing.SystemIcons]::WinLogo; $notify.visible = $true; $notify.showballoontip(10,'APP Updater','APP and its components have been updated to the latest version.',[system.windows.forms.tooltipicon]::None)" >nul 2>&1
-"C:\Program Files\LTSC\App.cmd"
+"%ltscdir%\App.cmd"
 exit
 
 
